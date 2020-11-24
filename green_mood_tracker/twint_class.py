39d0cec841a97ba0b7c0a5c099c8b770c1b7e2df
Leonardo Gavaudan <leg2173@columbia.edu>
2020-11-24 13:40:05 +0000

    can now search and save both for general queries and for city queries, cleaned up and renamed notebooks

--- green_mood_tracker/twint_class.py
+++ green_mood_tracker/twint_class.py
@@ -1,47 +1,90 @@
-# TWINT class
-
 import twint
+import pandas as pd
 
 
 class TWINT():
 
     def __init__(self, **kwargs):
         self.kwargs = kwargs
-
         self.username = kwargs.get("username", False)
-        self.search = kwargs.get("search", False)
-        self.store_csv = kwargs.get("store_csv", False)
+        self.keywords = kwargs.get("keywords", False)
+        self.since = kwargs.get("since", False)
+        self.cities = kwargs.get("cities", False)
+        self.file_path = kwargs.get("file_path", False)
         self.limit = kwargs.get("limit", False)
 
-    def save(self, file_path):
+    def csv(self):
         c = twint.Config()
 
         c.Username = self.username
         c.Lang = 'en'
-        c.Search = self.search
-
-        c.Store_csv = self.store_csv
-        c.Output = file_path
-
+        c.Hide_output = True
+        c.Search = self.keywords
+        c.Store_csv = True
+        c.Output = './' + self.file_path
         twint.run.Search(c)
 
-    def dataframe(self):
+    def search(self):
 
         c = twint.Config()
-
         c.Limit = self.limit
         c.Username = self.username
-        # c.Lang = 'en' To check out
-        c.Search = self.search
-
+        c.Hide_output = True
+        c.Search = self.keywords
         c.Pandas = True
         twint.run.Search(c)
 
         Tweets_df = twint.storage.panda.Tweets_df
-
         Tweets_df = Tweets_df[Tweets_df['language'] == 'en']
-
         return Tweets_df
+
+    def city_df(self):
+
+        self.cities = sorted(set(self.cities))
+
+        list_df = []
+
+        for city in self.cities:
+            c = twint.Config()
+            c.Limit = self.limit
+            c.Search = self.keywords
+            c.Since = self.since
+            c.Hide_output = True
+            c.Near = city
+            c.Pandas = True
+            twint.run.Search(c)
+
+            Tweets_df = twint.storage.panda.Tweets_df
+            Tweets_df = Tweets_df[Tweets_df['language'] == 'en']
+
+            list_df.append(Tweets_df)
+
+        df = pd.concat(list_df, ignore_index=True)
+        return df
+
+    def city_csv(self):
+
+        self.cities = sorted(set(self.cities))
+
+        list_df = []
+
+        for city in self.cities:
+            c = twint.Config()
+            c.Language = 'en'
+            c.Limit = self.limit
+            c.Search = self.keywords
+            c.Store_csv = True
+            c.Output = "./" + self.file_path
+            c.Since = self.since
+            c.Hide_output = True
+            c.Near = city
+            twint.run.Search(c)
+
+        csv = pd.read_csv('./' + self.file_path)
+        print(csv.columns[:2])
+        csv = csv[csv['language'] == 'en']
+        csv.drop([], axis=1, inplace=True)
+        csv.to_csv('./' + self.file_path, index=False)
 
 
 def main():
