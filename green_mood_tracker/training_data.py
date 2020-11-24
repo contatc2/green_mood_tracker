85b2968eaa65c088bb067f467160a2b3cd47dd95
Clementine Contat <clementine.contat@gmail.com>
2020-11-24 16:51:10 +0000

    added binary data extraction

--- green_mood_tracker/training_data.py
+++ green_mood_tracker/training_data.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 
-def get_raw_data(path = 'raw_data/'):
+def get_raw_data(path = 'raw_data/', binary=False):
 
     sts_gold = pd.read_csv(path + 'STS_gold.csv',sep=';')
     kaggle_sentiment_train = pd.read_csv(path + 'kaggle_sentiment_train.csv')
@@ -10,38 +10,42 @@ def get_raw_data(path = 'raw_data/'):
     sentiment140 = pd.read_csv(path + 'training.1600000.processed.noemoticon.csv',encoding='latin-1',header=None,names=column_names)
 
     sts_gold_final = sts_gold[['id','tweet','polarity']].rename(columns={'tweet':'text'})
-    sts_gold_final['polarity'] = sts_gold_final.polarity.map({4:2,0:0})
     sts_gold_final['source'] = "sts_gold"
 
-    kaggle_sentiment_train['polarity'] = kaggle_sentiment_train.sentiment.map({'positive':2,'neutral':1,'negative':0})
+    kaggle_sentiment_train['polarity'] = kaggle_sentiment_train.sentiment.map({'positive':4,'neutral':1,'negative':0})
     kaggle_sentiment_train_final = kaggle_sentiment_train[['textID','text','polarity']].rename(columns={'textID':'id'})
     kaggle_sentiment_train_final['source'] = "kaggle_sentiment_train"
 
-    kaggle_sentiment_test['polarity'] = kaggle_sentiment_test.sentiment.map({'positive':2,'neutral':1,'negative':0})
+    kaggle_sentiment_test['polarity'] = kaggle_sentiment_test.sentiment.map({'positive':4,'neutral':1,'negative':0})
     kaggle_sentiment_test_final = kaggle_sentiment_test[['textID','text','polarity']].rename(columns={'textID':'id'})
     kaggle_sentiment_test_final['source'] = "kaggle_sentiment_test"
 
     sentiment140_final = sentiment140[['id','text','polarity']]
-    sentiment140_final['polarity'] = sentiment140_final.polarity.map({4:2,0:0})
     sentiment140_final['source'] = 'sentiment140'
 
     complete_data = pd.concat([sts_gold_final,kaggle_sentiment_train_final,kaggle_sentiment_test_final,sentiment140_final])
     complete_data = complete_data.dropna()
 
+    if binary:
+        complete_data_binary = complete_data[complete_data.polarity != 1]
+        complete_data_binary['polarity'] = complete_data_binary.polarity.map({4:1,0:0})
+        return complete_data_binary
+
+    complete_data['polarity'] = complete_data.polarity.map({4:2,0:0})
     return complete_data
 
-def get_raw_data_notebook():
+def get_raw_data_notebook(binary=False):
 
     path = '../raw_data/'
-    return get_raw_data(path)
+    return get_raw_data(path, binary)
 
 
-def get_raw_data_colab():
+def get_raw_data_colab(binary=False):
 
     path = '/content/drive/My Drive/data_green_mood_tracker/'
-    return get_raw_data(path)
+    return get_raw_data(path, binary)
 
 
 if __name__ == '__main__':
-    raw_data = get_raw_data()
+    raw_data = get_raw_data(binary=True)
     print(raw_data.shape)
