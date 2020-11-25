ec135ff6c1a53437a798676e62209c8cf10734d8
Leonardo Gavaudan <leg2173@columbia.edu>
2020-11-25 18:36:35 +0000

    added cloud folder, and fixed some twint bugs as well created a fossil fuels dataset with 163 datapoints for us to label

--- green_mood_tracker/twint_class.py
+++ green_mood_tracker/twint_class.py
@@ -1,36 +1,29 @@
 import twint
 import pandas as pd
+import os
 
 
 class TWINT():
 
     def __init__(self, **kwargs):
         self.kwargs = kwargs
-        self.username = kwargs.get("username", False)
-        self.keywords = kwargs.get("keywords", False)
-        self.since = kwargs.get("since", False)
-        self.cities = kwargs.get("cities", False)
-        self.file_path = kwargs.get("file_path", False)
-        self.limit = kwargs.get("limit", False)
-
-    def csv(self):
-        c = twint.Config()
-
-        c.Username = self.username
-        c.Lang = 'en'
-        c.Hide_output = True
-        c.Search = self.keywords
-        c.Store_csv = True
-        c.Output = './' + self.file_path
-        twint.run.Search(c)
+        self.username = kwargs.get('username', False)
+        self.keywords = kwargs.get('keywords', False)
+        self.since = kwargs.get('since', False)
+        self.cities = kwargs.get('cities', False)
+        self.file_path = kwargs.get('file_path', False)
+        self.limit = kwargs.get('limit', False)
+        self.overwrite = kwargs.get('overwrite', True)
 
     def search(self):
-
         c = twint.Config()
+
         c.Limit = self.limit
-        c.Username = self.username
+        if self.username:
+            c.Username = self.username
         c.Hide_output = True
         c.Search = self.keywords
+        c.Since = self.since
         c.Pandas = True
         twint.run.Search(c)
 
@@ -38,10 +31,25 @@ class TWINT():
         Tweets_df = Tweets_df[Tweets_df['language'] == 'en']
         return Tweets_df
 
+    def csv(self):
+        if self.overwrite:
+            os.system(f'rm -f {self.file_path} 2> /dev/null')
+
+        c = twint.Config()
+
+        if self.username:
+            c.Username = self.username
+        c.Limit = self.limit
+        c.Hide_output = True
+        c.Store_csv = True
+        c.Output = './' + self.file_path
+        c.Search = self.keywords
+        c.Since = self.since
+
+        twint.run.Search(c)
+
     def city_df(self):
-
         self.cities = sorted(set(self.cities))
-
         list_df = []
 
         for city in self.cities:
@@ -63,27 +71,24 @@ class TWINT():
         return df
 
     def city_csv(self):
+        if self.overwrite:
+            os.system(f'rm -f {self.file_path} 2> /dev/null')
 
         self.cities = sorted(set(self.cities))
 
-        list_df = []
-
         for city in self.cities:
             c = twint.Config()
-            c.Language = 'en'
             c.Limit = self.limit
             c.Search = self.keywords
             c.Store_csv = True
-            c.Output = "./" + self.file_path
+            c.Output = './' + self.file_path
             c.Since = self.since
             c.Hide_output = True
             c.Near = city
             twint.run.Search(c)
 
         csv = pd.read_csv('./' + self.file_path)
-        print(csv.columns[:2])
         csv = csv[csv['language'] == 'en']
-        csv.drop([], axis=1, inplace=True)
         csv.to_csv('./' + self.file_path, index=False)
 
 
